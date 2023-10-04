db0b0458dc08396d1bb2e08315d315becf9d00cb
Rezz2040 <marcinrezler99@gmail.com>
2023-10-04 13:58:15 +0200

    session2 session3

--- session2.php
+++ session2.php
@@ -7,5 +7,5 @@
     echo 'Ulubione zwierzę: '. $_SESSION['animal'].'<br>';
     echo date('Y m d H:i:s'. $_SESSION['time']).'<br>';
 
-    echo $_SESSION['bzdura'];
+    echo '<br><a href="session3.php">Page 3</a>';
 ?>
